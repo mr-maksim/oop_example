3902f01a06266d9e98503b64887a98d649870654
mr-maksim <55948966+mr-maksim@users.noreply.github.com>
2022-03-17 19:29:23 +0300

    Added classes: * People * Mentor(People) * Reviewer(Mentor) * Lecturer(Mentor) * Student(People)

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-import __hello__
+class People:
+    pass
+
+
+class Mentor(People):
+    pass
+
+
+class Reviewer(Mentor):
+    pass
+
+
+class Lecturer(Mentor):
+    pass
+
+
+class Student(People):
+    pass
