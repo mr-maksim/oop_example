036a45110b57eeef6ed374393f808bed1f8fde64
mr-maksim <55948966+mr-maksim@users.noreply.github.com>
2022-03-18 21:25:08 +0300

    Added __str__ for classes: * Lecturer * Student

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class Mentor(People):
         super().__init__(name, sure_name)
         self.fixed_courses = []
 
+
 class Reviewer(Mentor):
     def __init__(self, name, sure_name):
         super().__init__(name, sure_name)
@@ -25,9 +26,23 @@ class Lecturer(Mentor):
         self.fixed_courses = []
         self.assessment = []
 
+    def __str__(self) -> str:
+        try:
+            score = sum(self.assessment)/len(self.assessment)
+        except:
+            score = "Лектор не получал оценки!"
+        return super().__str__() + f'\nСредняя оценка за лекции: {score}'
+
 
 class Student(People):
     def __init__(self, name, sure_name):
         super().__init__(name, sure_name)
         self.finished_courses = []
         self.assessment = []
+
+    def __str__(self) -> str:
+        try:
+            score = sum(self.assessment)/len(self.assessment)
+        except:
+            score = "Ученик не заработал ни одной оценки!"
+        return super().__str__() + f'\nСредняя оценка за домашние задания: {score}'
