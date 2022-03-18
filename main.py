faa2bcded20f152518a328584d806561b2b01235
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2022-03-18 21:34:57 +0300

    Adding new methods to the Mentor class.
    * add_course()
    * close_course()

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ class Mentor(People):
         super().__init__(name, sure_name)
         self.fixed_courses = []
 
+    def add_course(self, student, course_name):
+        student.courses.append(course_name)
+
+    def close_course(self, student, course_name):
+        if course_name in student.courses:
+            student.courses.remove(course_name)
+
 
 class Reviewer(Mentor):
     def __init__(self, name, sure_name):
@@ -46,3 +53,13 @@ class Student(People):
         except:
             score = "Ученик не заработал ни одной оценки!"
         return super().__str__() + f'\nСредняя оценка за домашние задания: {score}'
+
+
+some_reviewer_1 = Reviewer("Петр", "Петров")
+some_reviewer_2 = Reviewer("Иван", "Иванов")
+
+some_lecturer_1 = Lecturer("Вова", "Вовочкин")
+some_lecturer_2 = Lecturer("Василий", "Васильев")
+
+some_student_1 = Student("Татьяна", "Танечкина")
+some_student_2 = Student("Нина", "Ниночкина")
