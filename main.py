a5730b973d5842fbd8af0da563cbb10bcc2f7e7c
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2022-03-18 22:32:09 +0300

    Change in method add_course and close_course.

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ class People:
 class Mentor(People):
     def __init__(self, name, sure_name):
         super().__init__(name, sure_name)
-        self.fixed_courses = []
+        # self.fixed_courses = ['py']
 
     def add_course(self, student, course_name):
-        student.courses.append(course_name)
+        student.courses[course_name] = []
 
     def close_course(self, student, course_name):
         if course_name in student.courses:
-            student.courses.remove(course_name)
+            del student.courses[course_name]
 
 
 class Reviewer(Mentor):
@@ -26,12 +26,14 @@ class Reviewer(Mentor):
         super().__init__(name, sure_name)
         self.fixed_courses = []
 
+    def put_assessment(self, course_name, student):
+        pass
+
 
 class Lecturer(Mentor):
     def __init__(self, name, sure_name):
         super().__init__(name, sure_name)
-        self.fixed_courses = []
-        self.assessment = []
+        self.fixed_courses = {}
 
     def __str__(self) -> str:
         try:
@@ -44,22 +46,13 @@ class Lecturer(Mentor):
 class Student(People):
     def __init__(self, name, sure_name):
         super().__init__(name, sure_name)
-        self.finished_courses = []
-        self.assessment = []
+        self.courses = {}
 
     def __str__(self) -> str:
         try:
             score = sum(self.assessment)/len(self.assessment)
         except:
             score = "Ученик не заработал ни одной оценки!"
-        return super().__str__() + f'\nСредняя оценка за домашние задания: {score}'
+        return super().__str__() + f'\nСредняя оценка за домашние задания: {score} {self.courses}'
 
 
-some_reviewer_1 = Reviewer("Петр", "Петров")
-some_reviewer_2 = Reviewer("Иван", "Иванов")
-
-some_lecturer_1 = Lecturer("Вова", "Вовочкин")
-some_lecturer_2 = Lecturer("Василий", "Васильев")
-
-some_student_1 = Student("Татьяна", "Танечкина")
-some_student_2 = Student("Нина", "Ниночкина")
